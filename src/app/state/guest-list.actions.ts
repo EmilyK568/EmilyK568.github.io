0fa7e0dfd9c992b7cdb0dde273c16819d7ff6606
Emily Alderson <emilyalderson@MacBook-Pro-4.local>
2021-03-16 18:30:02 -0500

    Add remove logic to the state

--- src/app/state/guest-list.actions.ts
+++ src/app/state/guest-list.actions.ts
@@ -5,4 +5,9 @@ export const addGuests = createAction(
   '[Guest List] Add Guests',
   props<{ guests: Array<Guest> }>()
 );
+
+export const removeGuest = createAction(
+  '[Guest List] Remove Guest',
+  props<{ guests: Array<Guest> }>()
+);
  
--- src/app/state/guest-list.reducer.ts
+++ src/app/state/guest-list.reducer.ts
@@ -1,6 +1,6 @@
 import { createReducer, on, Action } from '@ngrx/store';
 import { Guest } from '../shared/interfaces/guest.interface';
-import { addGuests } from './guest-list.actions';
+import { addGuests, removeGuest } from './guest-list.actions';
 
 export const initialState: Array<Guest> = [
   {
@@ -13,5 +13,6 @@ export const initialState: Array<Guest> = [
 
 export const guestListReducer = createReducer(
   initialState,
-  on(addGuests, (state, { guests }) => state.concat(guests))
+  on(addGuests, (state, { guests }) => state.concat(guests)),
+  on(removeGuest, (state, { guests }) => state = guests)
 );
