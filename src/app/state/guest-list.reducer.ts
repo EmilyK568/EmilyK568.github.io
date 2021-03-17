2971b98a72fef908afdde09db7da4d2868e25e4b
Emily Alderson <emilyalderson@MacBook-Pro-4.local>
2021-03-16 21:20:46 -0500

    Update inital state value

--- src/app/state/guest-list.reducer.ts
+++ src/app/state/guest-list.reducer.ts
@@ -2,14 +2,7 @@ import { createReducer, on, Action } from '@ngrx/store';
 import { Guest } from '../shared/interfaces/guest.interface';
 import { addGuests, removeGuest } from './guest-list.actions';
 
-export const initialState: Array<Guest> = [
-  {
-    name: 'testing',
-    age: 12,
-    weight: 60,
-    friends: []
-  }
-];
+export const initialState: Array<Guest> = [];
 
 export const guestListReducer = createReducer(
   initialState,
