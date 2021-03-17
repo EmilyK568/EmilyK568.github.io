ecb634224ca7557daa86b48efa6213c062c0da84
Emily Alderson <emilyalderson@MacBook-Pro-4.local>
2021-03-16 22:31:19 -0500

    Add unit tests for guests component

--- src/app/features/guest-list/guests/guests.component.spec.ts
+++ src/app/features/guest-list/guests/guests.component.spec.ts
@@ -30,4 +30,52 @@ describe('GuestsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should remove a guest', () => {
+    component.sortedData = [{
+      name: 'Bill',
+      age: 23,
+      weight: 103,
+      friends: []
+    }];
+    const storeSpy = spyOn(component['store'], 'dispatch').and.returnValue(true);
+    component.removeGuest(0);
+    expect(storeSpy).toHaveBeenCalledTimes(1);
+    expect(component.sortedData).toEqual([]);
+  });
+
+  it('should sort the data by name', () => {
+    component.sortedData = [{
+      name: 'Bill',
+      age: 23,
+      weight: 103,
+      friends: []
+    },
+    {
+      name: 'Tim',
+      age: 45,
+      weight: 98,
+      friends: []
+    }];
+    const compareSpy = spyOn(component, <any>'compare').and.callThrough();
+    component.sortData({
+      active: 'name',
+      direction: 'desc'
+    });
+    expect(compareSpy).toHaveBeenCalledTimes(1);
+    expect(component.sortedData).toEqual([
+      {
+        name: 'Tim',
+        age: 45,
+        weight: 98,
+        friends: []
+      },
+      {
+      name: 'Bill',
+      age: 23,
+      weight: 103,
+      friends: []
+      }
+    ]);
+  });
 });
